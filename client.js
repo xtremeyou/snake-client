60f67f11015ebc767874171042438ca400e88a47
Merak Bourque <xtremeyou@hotmail.com>
2024-07-21 15:12:12 -0400

    added move command

--- client.js
+++ client.js
@@ -1,28 +1,30 @@
 const net = require("net");
 const connect = () => {
-    const conn = net.createConnection({
-      host: "localhost",
-      port: 50541
-    });
-    //once connection is estlablished sends string saying connected!
-    conn.on("connect", () => {
-        console.log("Connnected to the server!")
-    })
-    //outputs snk name lol
-    conn.on("connect", () => {
-        console.log("Sending my name your way!")
-        conn.write("Name: LOL")
-    })
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  //once connection is estlablished sends string saying connected!
+  conn.on("connect", () => {
+    console.log("Connnected to the server!");
+  });
+  //send our player username to the server
+  conn.on("connect", () => {
+    console.log("Sending my name your way!");
+    conn.write("Name: LOL");
+  });
 
+  //   conn.on("connect", () => {
+  //     conn.write("Move: up");
+  //   });
 
-    //turns data into readable txt
-    conn.setEncoding("utf-8");
-    //handles events, and in case handles the data the snake game outputs when you idle too long
-    conn.on("data", (data) => {
-      console.log("Server say:", data)
-  })
-  
-  
-    return conn;
-  };
-  module.exports = connect;
+  //turns data into readable txt
+  conn.setEncoding("utf-8");
+  //handles events, and in our case handles the data the snake game outputs when you idle too long
+  conn.on("data", (data) => {
+    console.log("Server say:", data);
+  });
+
+  return conn;
+};
+module.exports = connect;
