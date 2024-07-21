a48c76de0fe0078df4a45877cc49bfaa5bf68b6d
Merak Bourque <xtremeyou@hotmail.com>
2024-07-21 14:37:57 -0400

    added abiltiy to send snek game our player name: LOL

--- client.js
+++ client.js
@@ -4,6 +4,18 @@ const connect = () => {
       host: "localhost",
       port: 50541
     });
+    //once connection is estlablished sends string saying connected!
+    conn.on("connect", () => {
+        console.log("Connnected to the server!")
+    })
+    //outputs snk name lol
+    conn.on("connect", () => {
+        console.log("Sending my name your way!")
+        conn.write("Name: LOL")
+    })
+
+
+    //turns data into readable txt
     conn.setEncoding("utf-8");
     //handles events, and in case handles the data the snake game outputs when you idle too long
     conn.on("data", (data) => {
