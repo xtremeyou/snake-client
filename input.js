3505e8e485d126aa005a748e949b735f663d99ad
Merak Bourque <xtremeyou@hotmail.com>
2024-07-21 20:15:00 -0400

    added abiltiy to say a message to other players in game

--- input.js
+++ input.js
@@ -1,7 +1,8 @@
-let connection;
+let connection; //used as a global variable so that both functions can use it
+//
 
 //is an input module, thats used for play.js
-const setupInput = (conn) => {
+const setupInput = (conn) => { //passes conn variable to use in play.js
   connection = conn;
   const stdin = process.stdin; // create variable to hold the stdin object
   //so we don't have to type process.stdin multiple times
@@ -13,6 +14,10 @@ const setupInput = (conn) => {
   return stdin; // return the stdin object so we can use it elsewhere in the program
 };
 
+//connection.write uses the variable connect at the top of the page
+//connection is also passed into the function call on the play.js page allow us to use
+//the con object which is placed into the input function above.
+//this allows us to send controls to the server so we cna move the snek
 const handleUserInput = (key) => {
   if (key === "w") {
     connection.write("Move: up");
@@ -26,6 +31,11 @@ const handleUserInput = (key) => {
   if (key === "d") {
     connection.write("Move: right");
   }
+
+  if (key === "i") {
+    connection.write("Say: Can't catch me!")
+  }
+
   if (key === "\u0003") {
     process.exit();
   }
--- play.js
+++ play.js
@@ -2,6 +2,7 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-const conn = connect();
+const conn = connect(); //creates a variable out of connect so we can get the conn object and use it inside input.js
 
-setupInput(conn);
+setupInput(conn); //uses the object conn we assigned above to use the connection function and connect that to
+//the handleuserInoptus function
